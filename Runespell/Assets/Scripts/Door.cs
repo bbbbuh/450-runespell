7e5e7a9273a4dbad2a4979ece0253c9e57db8dd7
Ruby <114189866+bfp5355@users.noreply.github.com>
2025-03-04 11:08:08 -0500

    Added Door animations

---
+++ Runespell/Assets/Scripts/Door.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField]
+    private Animator animator;
+
+    [SerializeField]
+    private bool open;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void openDoor()
+    {
+
+    }
+}
